c0a94d272faac0cf72f10475d2a83a5c034acbd2
jeonghoon <shb03323@naver.com>
2022-11-05 22:14:46 +0900

    feat: 모델 설계 끝

--- meltingzone/src/main/java/com/meltingzone/meltingzone/domain/item/Character.java
+++ meltingzone/src/main/java/com/meltingzone/meltingzone/domain/item/Character.java
@@ -1,4 +1,18 @@
 package com.meltingzone.meltingzone.domain.item;
 
-public class Character {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("CHARACTER")
+@Getter
+@Setter
+public class Character extends Item {
+
+    @Column
+    private String characterUrl;
 }
--- meltingzone/src/main/java/com/meltingzone/meltingzone/domain/item/Music.java
+++ meltingzone/src/main/java/com/meltingzone/meltingzone/domain/item/Music.java
@@ -1,4 +1,18 @@
 package com.meltingzone.meltingzone.domain.item;
 
-public class Music {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("MUSIC")
+@Getter
+@Setter
+public class Music extends Item {
+
+    @Column
+    private String musicUrl;
 }
