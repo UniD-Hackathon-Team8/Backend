e56f00c895128fac28a689f93bf0fd7e4eeca7dc
jeonghoon <shb03323@naver.com>
2022-11-05 17:24:53 +0900

    init: initialization

---
+++ meltingzone/src/main/java/com/meltingzone/meltingzone/MeltingzoneApplication.java
@@ -0,0 +1,13 @@
+package com.meltingzone.meltingzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeltingzoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeltingzoneApplication.class, args);
+    }
+
+}
---
+++ meltingzone/src/test/java/com/meltingzone/meltingzone/MeltingzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meltingzone.meltingzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeltingzoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
